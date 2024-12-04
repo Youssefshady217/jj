17b55f2636f2163c61247749ca0d0424bacd73b5
Youssefshady217 <youssefshadymatta@gmail.com>
2024-12-04 12:16:59 +0200

    Update projectwescrabing.py

--- projectwescrabing.py
+++ projectwescrabing.py
@@ -8,7 +8,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-service = Service(executable_path="geckodriver.exe")
+service = Service(executable_path="geckodriver (2).exe")
 
 def init_driver(service):
     firefox_profile = webdriver.FirefoxProfile()
